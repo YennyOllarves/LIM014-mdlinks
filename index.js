8e5f6a9036196f8353f061cedbfb9731f4fcfe08
Yennys <yenny1989@gmail.com>
2021-03-21 19:26:30 -0500

    configurando entorno

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 module.exports = () => {
   // ...
 };
+
+
+
+const suma = (a, b) => new Promise((resolve, reject) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+  const sum = a + b;
+  resolve(sum);
+  }
+  reject(new Error('A y B tienen que ser de tipo number'))
+  });
+  
+  suma(3, 4)
+  .then(result => console.log(result))
+  .catch(err => console.log(err)); 
