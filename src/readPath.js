8af56ebdc116cb562d2a0c3638bb24d6ec5d5652
Yennys <yenny1989@gmail.com>
2021-04-07 10:22:33 -0500

    funcion para extraer links, falta test

--- src/readPath.js
+++ src/readPath.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const marked = require('marked');
 /* MIs rutas */
 
 const miRuta = '/home/laboratoria/LabProyectos/LIM014-mdlinks';
@@ -9,37 +10,63 @@ const miRuta = '/home/laboratoria/LabProyectos/LIM014-mdlinks';
 
 /* Verficar si existe una ruta */
 function miFuncion(route) {
-  if (fs.existsSync(route)) {
-    if (path.isAbsolute(route)) {
-      return route;
+  if (fs.existsSync(route)) { // SI mi ruta existe
+    if (path.isAbsolute(route)) { // Si mi ruta es absoluta
+      return route; // Retorname la ruta
     }
-    return path.resolve(route);
+    return path.resolve(route); // Sino entonces es realtiva, me la conviertes en absoluta.
   }
-  return console.log('Ruta no existente');
+  return console.log('Ruta no existente'); // SI NINGUNA DE LAS 2, PUES NO EXISTE.
 }
 
 /* console.log(miFuncion(rutaRelativa)); */
 
 function archivo() {
   const fnRuta = miFuncion(miRuta); // ALMACENA EL VALOR DE LA FUNCION.
-  console.log(fnRuta);
+  console.log('25', fnRuta);
   let result = [];
   if (fs.statSync(fnRuta).isDirectory() === true) {
     const listaArchivos = fs.readdirSync(fnRuta);
     listaArchivos.forEach((file) => {
-      if (path.extname(file) === '.md') {
-        result = result.concat(file);
+      const filePath = path.join(miRuta, file);
+      if (path.extname(filePath) === '.md') {
+        result = result.concat(filePath);
       }
     });
   } else {
-    return result;
+    console.log('35', result);
   }
+  console.log('37', result);
   return result;
 }
 
-console.log(archivo());
+console.log('40', archivo('/home/laboratoria/LabProyectos/LIM014-mdlinks'));
+
+function leerArchivo(route) {
+  const readFile = fs.readFileSync(route).toString();
+  return readFile;
+}
+console.log(leerArchivo('/home/laboratoria/LabProyectos/LIM014-mdlinks/README.md'));
+
+/* EXtraccion de arrays */
+const arrayLinks = (route) => {
+  const renderer = new marked.Renderer();
+  const readingFile = leerArchivo(route); // ALMACENA EL VALOR DE LA FUNCION.
+  let links = [];
+  renderer.link = (href, file, text) => { // Define la salida de propiedades
+    links = links.concat([{ href, text, path: route }]);
+  };
+  marked(readingFile, { renderer });
+
+  return links;
+};
+
+console.log(arrayLinks('/home/laboratoria/LabProyectos/LIM014-mdlinks/1.md'));
+
+
 
 module.exports = {
   miFuncion,
   archivo,
+  leerArchivo,
 };
